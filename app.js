dcaf0412070381355cb7701d1e10d26dbc111c8f
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-21 18:16:39 -0400

    added a function to find all documents

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
     url = 'mongodb://localhost:27017/mongodb-nodejs',
-    insertDocuments;
+    insertDocuments,
+    findDocuments;
 
 insertDocuments = function(db, callback) {
     var collection = db.collection('documents');
@@ -20,11 +21,25 @@ insertDocuments = function(db, callback) {
     });
 }
 
+findDocuments = function(db, callback) {
+    var collection = db.collection('documents');
+
+    collection.find({}).toArray(function(err, docs) {
+        assert.equal(null, err);
+        console.log('Found the following records:');
+        console.log(docs);
+
+        callback(docs);
+    });
+}
+
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log('Successfully connected to the server.');
 
     insertDocuments(db, function() {
-        db.close();
+        findDocuments(db, function() {
+            db.close();
+        });
     });
 });
